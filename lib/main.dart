361865841e34952f605a276fc532a9b92429937b
hassan-kodexo <107860797+hassan-kodexo@users.noreply.github.com>
2022-12-09 21:25:46 +0500

    working on statefullwidget

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,29 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
 
   void answerQuestion() {
-    print("Answer chosen!");
+    questionIndex = questionIndex + 1;
+    print(questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\s your name',
+      'What\s your favourite color',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -20,10 +34,13 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            Text('The question!'),
-            ElevatedButton(onPressed: answerQuestion, child: Text('Answer 1')),
-            ElevatedButton(onPressed: answerQuestion, child: Text('Answer 2')),
-            ElevatedButton(onPressed: answerQuestion, child: Text('Answer 3')),
+            Text(questions[0]),
+            ElevatedButton(
+                onPressed: () => answerQuestion(), child: Text('Answer 1')),
+            ElevatedButton(
+                onPressed: () => answerQuestion(), child: Text('Answer 2')),
+            ElevatedButton(
+                onPressed: () => answerQuestion(), child: Text('Answer 3')),
           ],
         ),
       ),
